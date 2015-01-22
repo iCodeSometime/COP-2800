d4e226ffaea81452b2d6456b96b0d4b29c1e9b48
Kenneth Cochran <kenneth.cochran101@gmail.com>
2015-01-22 17:02:17 -0600

    Finishing up project 2

--- src/project2/Project2.java
+++ src/project2/Project2.java
@@ -25,9 +25,17 @@ public class Project2 {
    }
 }
 class Person {
+   /*                           Class Constants                             */
+   // Beneath this BMI is underweight
+   private static final double UNDER_WEIGHT   = 18.5;
+   // Beneath this BMI is optimal weight
+   private static final double OPTIMAL_WEIGHT = 25.0;
+   // Beneath this BMI is overweight
+   private static final double OVER_WEIGHT    = 30.0;
    // Base prompt to get the input from user
-   private static final String INPUT_PROMPT =
+   private static final String INPUT_PROMPT   =
       "Please enter an integer ";
+   
    /*                          Member Variables                              */
    private int height;
    private int weight;
@@ -43,11 +51,11 @@ class Person {
       double BMI;       // The user's BMI
       String status; // The user's weight status
       BMI = calculateBMI(); 
-      if (BMI < 18.5) {
+      if (BMI < UNDER_WEIGHT) {
          status = "underweight";
-      } else if (BMI < 25.0) {
+      } else if (BMI < OPTIMAL_WEIGHT) {
          status = "at an optimal weight";
-      } else if (BMI < 30.0) {
+      } else if (BMI < OVER_WEIGHT) {
          status = "overweight";
       } else {
          status = "obese";
@@ -57,7 +65,7 @@ class Person {
    }
     
    /*                          Private Methods                               */
-   private int getValue(String item) {
+   private static int getValue(String item) {
       String  input;               // Used to store the input before processing
       String  units       = "";    // Units that go with the item we are finding
       boolean valid       = false; // Only true when a valid value is input
@@ -70,7 +78,10 @@ class Person {
          units = "pounds";
       } else {
          JOptionPane.showMessageDialog(null, 
-            "There has been an error in function 'getValue()'");
+            "There has been an error. Exiting Program.");
+         System.err.println("Error in function getValue(String item).");
+         System.err.println("Function has received an invalid parameter.");
+         System.err.println("Expected (height || weight) but received " + item);
          System.exit(1);
       }
       
