47a3102c3071f17b8370776193a1b6ca2f895768
ksc1077 <ksc1077@2152-P55072.college.local>
2015-03-05 13:52:06 -0600

    More testing

--- src/project5/Project5Test.java
+++ src/project5/Project5Test.java
@@ -11,11 +11,13 @@
 
 package project5;
 
+import javafx.stage.Stage;
+
 /**
  *
  * @author ksc1077
  */
-public class Project5Test {
+public class Project5Test extends Project5 {
    static {
       boolean assertionsEnabled = false;
       assert assertionsEnabled = true;
@@ -24,13 +26,60 @@ public class Project5Test {
             "This is a test class. Assertions must be enabled.");
       }
    }
+   @Override
+   public void start(Stage primaryStage) {
+      this.primaryStage = primaryStage;
+      this.primaryStage.setTitle("Cars Test");
+      this.primaryStage.setHeight(90);
+      this.primaryStage.setWidth(160);
+      
+      makeCar();
+      initRootLayout();
+      primaryStage.show();
+      testGUIAccelerate();
+      testGUIBrake();
+      System.out.println(CarTest.getFailedAssertions() 
+         + " failed GUI assertions");
+   }
+   
+   private void testGUIAccelerate() {
+      getCar().speed = 0;
+      try {
+         pushAccelerate();
+      } catch (UnsupportedOperationException e) {}
+      CarTest.fail(getCar().getSpeed() == Car.SPEED_QUANTIZATION,
+         "Accelerating is not supported. "
+         + "You should probably support that.");
+      getCar().speed = Car.SPEED_OF_LIGHT - Car.SPEED_QUANTIZATION;
+      try {
+         pushAccelerate();
+         CarTest.condescendingFail("Looks Like Einstein was wrong!!");
+      } catch (UnsupportedOperationException e) {}
+      CarTest.fail(getCar().getSpeed() == 
+         Car.SPEED_OF_LIGHT - Car.SPEED_QUANTIZATION,
+         "Looks like your code does weird stuff as it approaches c."
+       + "Sounds like it could be a relatively bad error.");
+   }
+   
+   private void testGUIBrake() {
+      
+      getCar().speed = Car.SPEED_QUANTIZATION;
+      pushBrake();
+      CarTest.fail(getCar().getSpeed() == 0,
+         "Your brakes are broken. Good luck.");
+      pushBrake();
+      CarTest.condescendingFail(getCar().getSpeed() == 0, 
+         "Awesome! You have magical brakes!");
+   }
+   
    public static void main(String[] args) {
       CarTest test = new CarTest();
-      System.out.println(test.runAllTests() + " assertions were failed.");
+      System.out.println(test.runAllTests() + " assertions failed in Car.");
+      launch(args);
    }
 }
 class CarTest extends Car {
-   private int failedAssertions;
+   private static int failedAssertions;
    public int runAllTests() {
       failedAssertions = 0;
       accelerate();
@@ -43,15 +92,17 @@ class CarTest extends Car {
       speed = 0;
       try {
          super.accelerate();
-      } catch (UnsupportedOperationException e) {
-         fail("Why is accelerating not supported?"
+      } catch (UnsupportedOperationException e) {}
+         fail(speed == SPEED_QUANTIZATION, "Why is accelerating not supported?"
             + "\nYou should probably support that.");
-      }
       speed = SPEED_OF_LIGHT - SPEED_QUANTIZATION;
       try {
          super.accelerate();
          condescendingFail("Looks like Einstein was wrong!!");
-      } catch(UnsupportedOperationException e){}
+      } catch(UnsupportedOperationException e) {}
+      fail(speed == SPEED_OF_LIGHT - SPEED_QUANTIZATION,
+         "Looks like your code does weird stuff as it approaches c."
+       + "Sounds like it could be a relatively bad error.");
       return getSpeed();
    }
    @Override
@@ -64,18 +115,21 @@ class CarTest extends Car {
       return getSpeed();
    }
    
-   /*          My own snarky custom fail method. Just for fun.               */
-   public void condescendingFail(String message) {
+   public static int getFailedAssertions() {return failedAssertions;}
+   
+   // Custom fail methods. 
+   // Used to add humor and keep track of failed assertions.
+   public static void condescendingFail(String message) {
       condescendingFail(false, message);
    }
-   public void condescendingFail(boolean passed, String message) {
+   public static void condescendingFail(boolean passed, String message) {
       fail(passed, message + "\n...or there's just a problem with your code."
          + "\nBetter check it.");
    }
-   public void fail(String message) {
+   public static void fail(String message) {
       fail(false, message);
    }
-   public void fail(boolean passed, String message) {
+   public static void fail(boolean passed, String message) {
       if (!passed) {
          failedAssertions += 1;
       }
