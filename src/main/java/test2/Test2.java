18dccf10b7704588b8a3990923e7e75febd846a3
ksc1077 <ksc1077@2152-P55072.college.local>
2015-03-24 13:54:22 -0500

    Test2

--- src/main/java/test2/Test2.java
+++ src/main/java/test2/Test2.java
@@ -10,14 +10,101 @@
 
 package test2;
 
-import java.util.Collection;
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import javafx.scene.*;
+import javafx.scene.layout.*;
 
-public class Test2 {
+import java.util.HashMap;
+import java.util.Set;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Test2 extends Application {
+    Stage primaryStage;
+    FlowPane rootLayout;
+    Garage garage;
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        garage = new Garage();
+        
+        initRootLayout();
+        insertMetadataInput(rootLayout);
+        insertServices(rootLayout);
+        insertButtons(rootLayout);
+        
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+    
+    private void initRootLayout() {
+        rootLayout = new FlowPane();
+        primaryStage.setScene(new Scene(new AnchorPane(rootLayout)));
+    }
+    private void insertMetadataInput(Pane parent) {
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+        parent.getChildren().add(hbox);
+        
+        HBox nameHBox = new HBox();
+        TextField id = new TextField();
+        id.setId("customerID");
+        nameHBox.getChildren().add(new Label("Customer ID: "));
+        nameHBox.getChildren().add(id);
+        
+        HBox dateHBox = new HBox();
+        DatePicker date = new DatePicker();
+        date.setId("date");
+        dateHBox.getChildren().add(new Label("Date: "));
+        dateHBox.getChildren().add(date);
+        
+        hbox.getChildren().add(nameHBox);
+        hbox.getChildren().add(dateHBox);
+        
+        
+    }
+    private void insertServices(Pane parent) {
+        VBox vbox = new VBox();
+        vbox.setSpacing(10);
+        parent.getChildren().add(vbox);
+        // Get the services list and insert each into the FlowPane
+        Garage.getServices().forEach((service) -> {
+            CheckBox box = new CheckBox();
+            box.setText(service + " - $" + Garage.getPrice(service));
+            box.setId(service.replaceAll("\\s", ""));
+            vbox.getChildren().add(box);
+        });
+    }
+    private void insertButtons(Pane parent) {
+        Button addService = new Button();
+        addService.setText("Submit Order");
+        addService.onActionProperty().set((event) -> {
+            TextField idField = (TextField)rootLayout.lookup("#customerID");
+            String id = idField.getText();
+            idField.setText("");
+            
+            TextField dateField = (TextField)rootLayout.lookup("#date");
+            String date = dateField.getText();
+            dateField.setText("");
+            
+        });
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
     
 }
 class Garage {
-    private static final String[][] orders = new String[5][3];
+    //static
+    private static final ArrayList<ArrayList<String>> orders = new ArrayList<>();
     private static final HashMap<String, String> services =
             new HashMap<>();
     static {
@@ -26,9 +113,15 @@ class Garage {
         services.put("Random other service", "102.80");
     }
     
-    public Collection getServices() { return services.values(); }
-    public String     getPrice(String service) {
-        return services.get(service);
+    public static Set<String> getServices() { return services.keySet(); }
+    public static String     getPrice(String service) {
+        String temp = services.getOrDefault(service, "");
+        System.out.println("Getting "  + service + " from hash map.");
+        System.out.println("Returned " + temp);
+        return temp;
     }
-    
+    //end static
+    public void addOrder(String id, LocalDateTime time, String price) {
+        ArrayList<String> order = new ArrayList<>();
+    }
 }
